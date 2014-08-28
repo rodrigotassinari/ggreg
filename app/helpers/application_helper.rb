3ce1411e712436781e3b87b2cc540050c3ba625a
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2014-08-28 19:47:45 -0300

    Makes site name configurable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
 
+  def app_name
+    ENV['APP_NAME'] || 'GGreg'
+  end
+
   def title
-    content_for?(:title) ? content_for(:title).concat(' :: GGreg') : 'GGreg'
+    content_for?(:title) ? content_for(:title).concat(" :: #{app_name}") : app_name
   end
 
   def description
-    content_for(:description) || 'GGreg description' # TODO
+    content_for(:description) || "#{app_name} description" # TODO
   end
 
 end
